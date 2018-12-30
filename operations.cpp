ae5c5239856e094df2f55515174965a62a258447
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-12-30 16:48:13 +0900

    リファクタリング。

--- operations.cpp
+++ operations.cpp
@@ -22,11 +22,6 @@
 
 #include <iostream>
 
-// int (*getattr) (const char *, struct stat *);
-// int (*readdir) (const char *, void *, fuse_fill_dir_t, off_t, struct fuse_file_info *);
-// int (*open) (const char *, struct fuse_file_info *);
-// int (*read) (const char *, char *, size_t, off_t, struct fuse_file_info *);
-
 #define DROMOZOA_SET_OPERATION(name) \
   operations.name = name \
   /**/
