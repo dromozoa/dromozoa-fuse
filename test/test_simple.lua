703e140d8bf429396ce46b52346814d44c7eab5e
Tomoyuki Fujimori <moyu@dromozoa.com>
2019-01-11 18:34:43 +0900

    テストを修正。

--- test/test_simple.lua
+++ test/test_simple.lua
@@ -33,16 +33,50 @@ local function mode_dir(mode)
   return unix.bor(unix.S_IFDIR, tonumber(mode, 8))
 end
 
+local function mode_file(mode)
+  return unix.bor(unix.S_IFREG, tonumber(mode, 8))
+end
+
+local function is_empty_dir(node)
+  local m = 0
+  local n = 0
+  for name in pairs(node.nodes) do
+    m = m + 1
+    if name == "." or name == ".." then
+      n = n + 1
+    end
+    if m > 2 then
+      return false
+    end
+  end
+  return m == 2 and n == 2
+end
+
 local function update_atime(node)
   node.attr.st_atime = current_time
+  return node
 end
 
 local function update_mtime(node)
   node.attr.st_mtime = current_time
+  return node
 end
 
 local function update_ctime(node)
   node.attr.st_ctime = current_time
+  return node
+end
+
+local function increment_link(node)
+  local attr = node.attr
+  attr.st_nlink = attr.st_nlink + 1
+  return update_ctime(node)
+end
+
+local function decrement_link(node)
+  local attr = node.attr
+  attr.st_nlink = attr.st_nlink - 1
+  return update_ctime(node)
 end
 
 local vfs = {}
@@ -118,11 +152,7 @@ local function set(path, node)
 end
 
 local function link(oldpath, newpath)
-  local this = get(oldpath)
-  local attr = this.attr
-  attr.st_nlink = attr.st_nlink + 1
-  attr.st_ctime = current_time;
-  set(newpath, this)
+  set(newpath, increment_link(get(oldpath)))
 end
 
 link("/", "/.")
@@ -131,6 +161,40 @@ local operations = {}
 
 function operations:getattr(path)
   return get(path).attr
+end
+
+function operations:unlink(path)
+  local node = get(path)
+  local parent_path, name = split(path)
+  local parent_node = get(parent_path)
+  update_current_time()
+  set(path)
+  decrement_link(node)
+  update_mtime(parent_node)
+  return 0
+end
+
+function operations:rmdir(path)
+  local node = get(path)
+  local parent_path, name = split(path)
+  local parent_node = get(parent_path)
+
+  if unix.band(node.attr.st_mode, unix.S_IFDIR) == 0 then
+    error(-unix.ENOTDIR)
+  end
+  if not is_empty_dir(node) then
+    error(-unix.ENOTEMPTY)
+  end
+
+  update_current_time()
+  set(path .. "/..")
+  set(path .. "/.")
+  set(path)
+  decrement_link(node)
+  decrement_link(node)
+  decrement_link(parent_node)
+  update_mtime(parent_node)
+  return 0
 end
 
 function operations:mkdir(path)
@@ -156,6 +220,30 @@ function operations:mkdir(path)
   return 0
 end
 
+function operations:read(path, size, offset)
+  local node = get(path)
+  local content = node.content
+  local buffer = {}
+  for i = 1, size do
+    buffer[i] = content[offset + i]
+  end
+  print(("READ %s %d %d %q"):format(path, size, offset, table.concat(buffer)))
+  return table.concat(buffer)
+end
+
+
+function operations:write(path, buffer, offset)
+  local node = get(path)
+  local content = node.content
+  local n = #buffer
+  for i = 1, n do
+    content[offset + i] = buffer:sub(i, i)
+  end
+  print(("WRITE %s %d %d %q"):format(path, #buffer, offset, table.concat(content)))
+  node.attr.st_size = #content
+  return n
+end
+
 function operations:statfs(path)
   return vfs
 end
@@ -168,239 +256,42 @@ function operations:readdir(path, fill)
   return 0
 end
 
+function operations:create(path, mode)
+  local parent_path, name = split(path)
+  update_current_time()
+  set(path, {
+    attr = {
+      st_mode = mode_file "0644";
+      st_nlink = 1;
+      st_uid = uid;
+      st_gid = gid;
+      st_atime = current_time;
+      st_mtime = current_time;
+      st_ctime = current_time;
+      st_blocks = 0;
+    };
+    content = {};
+  })
+  return 0
+end
+
+function operations:ftruncate(path, size)
+  local node = get(path)
+  local content = node.content
+  local n = #content
+  if n < size then
+    for i = n + 1, size do
+      content[i] = "\0"
+    end
+  else
+    for i = size + 1, n do
+      content[i] = nil
+    end
+  end
+  node.attr.st_size = #content
+  return 0
+end
+
+
 local result = fuse.main({ arg[0], ... }, operations)
 assert(result == 0)
-
---[====[
-
-
-local root = {
-  {
-    st_mode = unix.bor(unix.S_IFDIR, tonumber("0755", 8));
-    st_nlink = 1;
-    st_uid = uid;
-    st_gid = gid;
-    st_atime = now;
-    st_mtime = now;
-    st_ctime = now;
-    st_blocks = 0;
-  }
-}
-
-local function parse(path)
-  if path == "/" then
-    return nil, "/"
-  else
-    local parent_path, name = path:match "^(.*)/([^/]+)$"
-    if #parent_path == 0 then
-      return "/", name
-    else
-      return parent_path, name
-    end
-  end
-end
-
-local function get(path)
-  if path == "/" then
-    return root
-  else
-    local items = {}
-    for item in path:gmatch "/([^/]+)" do
-      items[#items + 1] = item
-    end
-    local this = root
-    local n = #items
-    for i = 1, n - 1 do
-      this = this[items[i]]
-      if not this then
-        return
-      end
-    end
-    return this[items[n]]
-  end
-end
-
-local function set(path, data)
-  local items = {}
-  for item in path:gmatch "/([^/]+)" do
-    items[#items + 1] = item
-  end
-  local this = root
-  local n = #items
-  for i = 1, n - 1 do
-    this = this[items[i]]
-  end
-  if data then
-    this[items[n]] = { data }
-  else
-    this[items[n]] = nil
-  end
-end
-
-local function unlink(path)
-  local data = get(path)[1]
-  data.st_nlink = data.st_nlink - 1
-  data.st_atime = now
-  data.st_mtime = now
-end
-
-local function link(source, result)
-  local data = get(source)[1]
-  data.st_nlink = data.st_nlink + 1
-  data.st_atime = now
-  data.st_mtime = now
-  set(result, data)
-end
-
-root[".."] = {}
-link("/", "/.")
-
-local ops = {}
-
-function ops:getattr(path)
-  local this = get(path)
-  if not this then
-    return -unix.ENOENT
-  end
-  return this[1]
-end
-
-function ops:readlink(path)
-  local this = get(path)
-  if not this then
-    return -unix.ENOENT
-  end
-  if unix.band(this[1].st_mode, unix.S_IFLNK) == 0 then
-    return -unix.EINVAL
-  end
-  return this[1][1]
-end
-
-function ops:mkdir(path)
-  local parent_path, name = parse(path)
-  if not parent_path then
-    return -unix.EEXIST
-  end
-  local parent = get(parent_path)
-  if not parent then
-    return -unix.ENOENT
-  end
-  if unix.band(parent[1].st_mode, unix.S_IFDIR) == 0 then
-    return -unix.ENOTDIR
-  end
-  if get(path) then
-    return -unix.EEXIST
-  end
-
-  now = os.time()
-  set(path, {
-    st_mode = unix.bor(unix.S_IFDIR, tonumber("0755", 8));
-    st_nlink = 1;
-    st_uid = uid;
-    st_gid = gid;
-    st_atime = now;
-    st_mtime = now;
-    st_ctime = now;
-    st_blocks = 0;
-  })
-
-  link(parent_path, path .. "/..")
-  link(path, path .. "/.")
-
-  return 0
-end
-
-function ops:rmdir(path)
-  local this = get(path)
-  if unix.band(this[1].st_mode, unix.S_IFDIR) == 0 then
-    return -unix.ENOTDIR
-  end
-  for name in pairs(this) do
-    if type(name) == "string" and name ~= "." and name ~= ".." then
-      return -unix.ENOTEMPTY
-    end
-  end
-
-  now = os.time()
-  unlink(path .. "/..")
-  set(path)
-  return 0
-end
-
-function ops:symlink(source, result)
-  print("!", source, result)
-
-  local parent_path, name = parse(result)
-  if not parent_path then
-    return -unix.EEXIST
-  end
-  local parent = get(parent_path)
-  if not parent then
-    return -unix.ENOENT
-  end
-  if unix.band(parent[1].st_mode, unix.S_IFDIR) == 0 then
-    return -unix.ENOTDIR
-  end
-  if get(result) then
-    return -unix.EEXIST
-  end
-
-  now = os.time()
-  set(result, {
-    st_mode = unix.bor(unix.S_IFLNK, tonumber("0777", 8));
-    st_nlink = 1;
-    st_uid = uid;
-    st_gid = gid;
-    st_atime = { tv_sec = now, tv_nsec = 123456789 };
-    st_mtime = { tv_sec = now, tv_nsec = 424242424 };
-    st_ctime = { tv_sec = now, tv_nsec = 696969696 };
-    st_blocks = 0;
-    st_size = #source;
-    source;
-  })
-
-  -- TODO update directory entry?
-
-  return 0
-end
-
-function ops:read(path, size, offset, info)
-  return -unix.ENOTSUP
-end
-
-function ops:write(path, data, offset, info)
-  return -unix.ENOTSUP
-end
-
-function ops:statfs(path)
-  return {
-    f_bsize = 512;
-    f_frsize = 512;
-    f_blocks = 1024;
-    f_bfree = 1024;
-    f_bavail = 1024;
-    f_files = 1024;
-    f_ffree = 1000;
-    f_namemax = 256;
-  }
-end
-
-function ops:opendir()
-  return 0
-end
-
-function ops:readdir(path, fill, offset, info)
-  local this = get(path)
-  for name, value in pairs(this) do
-    if type(name) == "string" then
-      fill(name)
-    end
-  end
-  return 0
-end
-
-function ops:closedir()
-  return 0
-end
-
-]====]
